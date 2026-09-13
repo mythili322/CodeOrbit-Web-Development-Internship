8ebcd3a27c1c68b0694f27a40729973813bfb8e6
Mythili Suryavamsham <suryavamshammythili@gmail.com>
2026-09-13 10:23:45 +0530

    Add Responsive Landing Page project

---
+++ Responsive Landing Page/script.js
@@ -0,0 +1,15 @@
+function openCourse(courseName) {
+
+    if (courseName === "python") {
+        alert("🐍 Python Programming\n\nLearn Python from basics and build real-world projects.");
+    }
+
+    else if (courseName === "ai") {
+        alert("🧠 AI & Machine Learning\n\nLearn Artificial Intelligence and Machine Learning concepts.");
+    }
+
+    else if (courseName === "data") {
+        alert("📊 Data Analytics\n\nLearn data analysis, visualization and useful insights.");
+    }
+
+}
